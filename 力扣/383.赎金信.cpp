f44f99cbea81094752e0db0c6b96f7d283616c51
zhu-to-u <2528487925@qq.com>
2023-11-01 20:30:59 +0800

    简单的哈希表

---
+++ 力扣/383.赎金信.cpp
@@ -0,0 +1,34 @@
+/*
+ * @lc app=leetcode.cn id=383 lang=cpp
+ *
+ * [383] 赎金信
+ */
+
+// @lc code=start
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        int record[26]={0};
+        for(int i=0;i<ransomNote.size();i++)
+        {
+            record[ransomNote[i]-'a']++;
+        }
+        for(int i=0;i<magazine.size();i++)
+        {
+            record[magazine[i]-'a']--;
+        }
+
+        for(int i=0;i<26;i++)
+        {
+            if(record[i]>0)
+            {
+                return false;
+            }
+        }
+
+        return true;
+
+    }
+};
+// @lc code=end
+
