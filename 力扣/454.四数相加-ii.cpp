5855ac78026394886be7b4c73603db8a63b85626
zhu-to-u <2528487925@qq.com>
2023-11-03 22:11:42 +0800

    unordered_map

---
+++ 力扣/454.四数相加-ii.cpp
@@ -0,0 +1,37 @@
+/*
+ * @lc app=leetcode.cn id=454 lang=cpp
+ *
+ * [454] 四数相加 II
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int fourSumCount(vector<int>& nums1, vector<int>& nums2, vector<int>& nums3, vector<int>& nums4) {
+        unordered_map<int,int> umap;
+        for(int a:nums1)
+        {
+            for(int b:nums2)
+            {
+                umap[a+b]++;
+            }
+        }
+
+        int count=0;
+        for(int c:nums3)
+        {
+            for(int d:nums4)
+            {
+                if(umap.find(0-(c+d))!=umap.end())
+                {
+                    count+=umap[0-(c+d)];
+                }
+            }
+        }
+
+        return count;
+
+    }
+};
+// @lc code=end
+
