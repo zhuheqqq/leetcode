802d3a825bb64e61d147952a0caea5e5f4ad06e1
zhu-to-u <2528487925@qq.com>
2023-12-10 12:51:08 +0800

    N叉树的前序遍历

---
+++ 力扣/590.cpp
@@ -0,0 +1,43 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+public:
+
+    void helper(const Node* root,vector<int>& res)
+    {
+        if(root==nullptr)
+        {
+            return;
+        }
+        for(auto& ch:root->children)
+        {
+            helper(ch,res);
+        }
+        res.emplace_back(root->val);
+    }
+
+    vector<int> postorder(Node* root) {
+        vector<int> res;
+        helper(root,res);
+        return res;
+        
+    }
+};
