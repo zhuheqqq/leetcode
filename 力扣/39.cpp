c447bc70c80e6b6f4f7477d090e43e7012658831
zhuheqqq <zhuheqin55@gmail.com>
2024-07-22 09:28:55 +0800

    回溯经典套路:排序+剪枝

--- 力扣/39.cpp
+++ 力扣/39.cpp
@@ -25,3 +25,34 @@ public:
         return ans;
     }
 };
+
+//优化之后版本排序+剪枝
+class Solution {
+private:
+    vector<vector<int>> result;
+    vector<int> path;
+    void backtracking(vector<int>& candidates, int target, int sum, int startIndex) {
+        if (sum == target) {
+            result.push_back(path);
+            return;
+        }
+
+        // 如果 sum + candidates[i] > target 就终止遍历
+        for (int i = startIndex; i < candidates.size() && sum + candidates[i] <= target; i++) {
+            sum += candidates[i];
+            path.push_back(candidates[i]);
+            backtracking(candidates, target, sum, i);
+            sum -= candidates[i];
+            path.pop_back();
+
+        }
+    }
+public:
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        result.clear();
+        path.clear();
+        sort(candidates.begin(), candidates.end()); // 需要排序
+        backtracking(candidates, target, 0, 0);
+        return result;
+    }
+};
