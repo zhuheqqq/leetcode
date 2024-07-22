5b812c0fa86fc8858c441019982ab8aaba4462b6
zhuheqqq <zhuheqin55@gmail.com>
2024-07-22 09:56:42 +0800

    回溯,横向遍历不允许有重复的组合

---
+++ 力扣/40.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<vector<int>> ans;
+    vector<int> path;
+
+    void combinationSum2Helper(vector<int>& candidates, int target, int sum, int start){
+        if(sum == target){
+            ans.push_back(path);
+            return;
+        }
+        for(int i = start; i < candidates.size(); i++){
+            if(i > start && candidates[i] == candidates[i - 1]){
+                continue;
+            }
+            sum += candidates[i];
+            path.push_back(candidates[i]);
+            combinationSum2Helper(candidates, target, sum, i + 1);
+            sum -= candidates[i];
+            path.pop_back();
+        }
+    }
+
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        sort(candidates.begin(), candidates.end());
+        combinationSum2Helper(candidates, target, 0, 0);
+        return ans;
+    }
+};
