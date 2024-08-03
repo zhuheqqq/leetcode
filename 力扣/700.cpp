0f6e051a3c9b03ca76ea79c5c8d9e571a77b2667
zhuheqqq <zhuheqin55@gmail.com>
2024-08-03 11:12:12 +0800

    递归

---
+++ 力扣/700.cpp
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* searchBST(TreeNode* root, int val) {
+        if(root == NULL || root->val == val){
+            return root;
+        }
+        TreeNode* ans = NULL;
+        if(root->val > val){
+            ans = searchBST(root->left, val);
+        }
+        if(root->val < val) {
+            ans = searchBST(root->right, val);
+        }
+        return ans;
+
+    }
+};
